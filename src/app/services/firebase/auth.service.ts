bc7028dacb0b868d27b1c0738eaeb57ac176d53b
jorojassoto <josevrojasoto@gmail.com>
2024-12-09 18:07:00 -0300

    agregado métodos de inicio de sesión con Google y GitHub en el servicio de autenticación

--- src/app/services/firebase/auth.service.ts
+++ src/app/services/firebase/auth.service.ts
@@ -2,24 +2,47 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { map, Observable } from 'rxjs';
+import firebase from 'firebase/compat/app';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class AuthService {
   constructor(
     private angularFireAuth: AngularFireAuth,
-    private angularFirestore: AngularFirestore
-  ) {}
+    private firestore: AngularFirestore
+  ) { }
+
+  login(email: string, pass: string) {
+    return this.angularFireAuth.signInWithEmailAndPassword(email, pass);
+  }
+
+  loginWithGoogle() {
+    return this.angularFireAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .catch(error => {
+        console.error('Error al autenticar con Google: ', error);
+        throw error;
+      })
+  }
+
+  loginWithGitHub() {
+    const provider = new firebase.auth.GithubAuthProvider();
+    return this.angularFireAuth.signInWithPopup(provider)
+      .then(result => {
+        console.log('Autenticado con GitHub: ', result);
+        return result;
+      })
+      .catch(error => {
+        console.error('Error al autenticar con GitHub: ', error);
+        throw error;
+      })
+  }
 
   isLogged(): Observable<any> {
     return this.angularFireAuth.authState.pipe(
       map((user) => (user ? user.uid : null))
     );
-  }
-
-  login(email: string, pass: string) {
-    return this.angularFireAuth.signInWithEmailAndPassword(email, pass);
   }
 
   register(email: string, pass: string) {
